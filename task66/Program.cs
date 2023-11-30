657dd37912f47d5cbdfa2ff881ca0f2abb8acf51
Maxkwizi <maxkwizi@inbox.ru>
2023-11-30 13:57:45 +0500

    AllTaskDone

---
+++ task66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Sum (int start, int end)
+{
+    if (start == end) return start;
+    return start + Sum (start + 1, end);
+}
+Console.WriteLine($"M = {M}; N = {N} -> {Sum (M, N)}");
---
+++ task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// n = 3, m = 2 -> A(n,m) = 29
+
+
+Console.WriteLine("Введите число n");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число m");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Ack(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if ((n != 0 ) && (m == 0)) return Ack(n - 1, 1);
+    else return Ack(n - 1, Ack(n, m - 1));    
+}
+Console.WriteLine($"N = {a}; M = {b} -> {Ack(a, b)}");
